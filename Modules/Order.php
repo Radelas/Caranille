066aecf98eded7fff6348461de2b27613b5b7e56
Caranille <hyppothalus@gmail.com>
2014-10-02 14:24:09 +0200

    Encore un bug

--- Modules/Order.php
+++ Modules/Order.php
@@ -5,55 +5,57 @@
 	{
 		if ($_SESSION['Order_ID'] == 1)
 		{
-			echo '<p>Bienvenue sur la page des ordres</p>';
-			echo '<p>Vous êtes actuellement neutre. Pour participer au PVP dans le champs de batailles vous devez choisir un odre à servir</p>';
-			echo '<p>ATTENTION, ce choix est irréversible, choisissez donc bien</p>';
-
-			echo '<p>Voici les deux ordres disponibles. Choisissez bien</p>';
-			$Data_Order_Query = $bdd->query("SELECT * FROM Caranille_Orders WHERE Order_ID != 1");
-			echo '<table>';
-		
-					echo '<tr>';
-
-						echo '<td>';
-							echo 'Nom';
-						echo '</td>';
-
-						echo '<td>';
-							echo 'Description';
-						echo '</td>';
-
-						echo '<td>';
-							echo 'Action';
-						echo '</td>';
-
-					echo '</tr>';
-					
-			while ($Order_Data = $Data_Order_Query->fetch())
+			if(empty($_POST['Accept']))
 			{
-				echo '<tr>';
-						
-						$Order_ID = stripslashes($Order_Data['Order_ID']);
-						
-						echo '<td>';
-							echo '' .stripslashes($Order_Data['Order_Name']). '';
-						echo '</td>';
-					
-						echo '<td>';
-							echo '' .stripslashes(nl2br($Order_Data['Order_Description'])). '';
-						echo '</td>';
-										
-						echo '<td>';
-							echo '<form method="POST" action="Order.php">';
-							echo "<input type=\"hidden\" name=\"Order_ID\" value=\"$Order_ID\">";
-							echo '<input type="submit" name="Accept" value="Rejoindre">';
-							echo '</form><br />';
-						echo '</td>';
-					
-					echo '</tr>';
-			}
-			echo '</table>';
+				echo '<p>Bienvenue sur la page des ordres</p>';
+				echo '<p>Vous êtes actuellement neutre. Pour participer au PVP dans le champs de batailles vous devez choisir un odre à servir</p>';
+				echo '<p>ATTENTION, ce choix est irréversible, choisissez donc bien</p>';
+
+				echo '<p>Voici les deux ordres disponibles. Choisissez bien</p>';
+				$Data_Order_Query = $bdd->query("SELECT * FROM Caranille_Orders WHERE Order_ID != 1");
+				echo '<table>';
 			
+						echo '<tr>';
+
+							echo '<td>';
+								echo 'Nom';
+							echo '</td>';
+
+							echo '<td>';
+								echo 'Description';
+							echo '</td>';
+
+							echo '<td>';
+								echo 'Action';
+							echo '</td>';
+
+						echo '</tr>';
+						
+				while ($Order_Data = $Data_Order_Query->fetch())
+				{
+					echo '<tr>';
+							
+							$Order_ID = stripslashes($Order_Data['Order_ID']);
+							
+							echo '<td>';
+								echo '' .stripslashes($Order_Data['Order_Name']). '';
+							echo '</td>';
+						
+							echo '<td>';
+								echo '' .stripslashes(nl2br($Order_Data['Order_Description'])). '';
+							echo '</td>';
+											
+							echo '<td>';
+								echo '<form method="POST" action="Order.php">';
+								echo "<input type=\"hidden\" name=\"Order_ID\" value=\"$Order_ID\">";
+								echo '<input type="submit" name="Accept" value="Rejoindre">';
+								echo '</form><br />';
+							echo '</td>';
+						
+						echo '</tr>';
+				}
+				echo '</table>';
+			}	
 			if (isset($_POST['Accept']))
 			{
 				$Order_ID = htmlspecialchars(addslashes($_POST['Order_ID']));
