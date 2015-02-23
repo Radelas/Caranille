e524ba165752945cbe557c82505f73db2ff4df7d
Caranille <mycaranille@gmail.com>
2015-02-23 14:52:21 +0100

    Update Register.php

--- Modules/Register.php
+++ Modules/Register.php
@@ -21,54 +21,18 @@
 	}	
 	if (isset($_POST['Register']))
 	{
-		$Account_Pseudo = htmlspecialchars(addslashes($_POST['Account_Pseudo']));
-		$Account_Password = htmlspecialchars(addslashes($_POST['Account_Password']));
 		if (isset($_POST['Pseudo']) && ($_POST['Password']) && ($_POST['Email']))
 		{
+			$Account_Pseudo = htmlspecialchars(addslashes($_POST['Account_Pseudo']));
 			$Account_Password = htmlspecialchars(addslashes($_POST['Account_Password']));
 			$Account_Password_Confirm = htmlspecialchars(addslashes($_POST['Account_Password_Confirm']));
 			$Account_Email = htmlspecialchars(addslashes($_POST['Account_Email']));
+			$Account_Password = md5(htmlspecialchars(addslashes($_POST['Password'])));
 			if ($Account_Password == $Account_Password_Confirm)
 			{
 				if (isset($_POST['Licence']))
 				{
 					SQL_Add_Account($Account_Pseudo, $Account_Password, $Account_Email);
-					$Pseudo_List_Query = $bdd->prepare("SELECT * FROM Caranille_Accounts WHERE Account_Pseudo= ?");
-					$Pseudo_List_Query->execute(array($_POST['Pseudo']));
-
-					$Pseudo_List = $Pseudo_List_Query->rowCount();
-					if ($Pseudo_List == 0)
-					{
-						$Pseudo = htmlspecialchars(addslashes($_POST['Pseudo']));
-						$Password = md5(htmlspecialchars(addslashes($_POST['Password'])));
-						$Email = htmlspecialchars(addslashes($_POST['Email']));
-						$Date = date('Y-m-d H:i:s');
-						$IP = $_SERVER["REMOTE_ADDR"];
-						$Add_Account = $bdd->prepare("INSERT INTO Caranille_Accounts VALUES('', '0', :Pseudo, :Password, :Email, '1', '100', '0', '10', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', 'Member', :Date, :IP, 'Authorized' , 'None')");
-						$Add_Account->execute(array('Pseudo' => $Pseudo, 'Password' => $Password, 'Email' => $Email, 'Date' => $Date, 'IP' => $IP));
-							
-
-						$Account_Data_Query = $bdd->prepare("SELECT * FROM Caranille_Accounts 
-						WHERE Account_Pseudo= ?");
-						$Account_Data_Query->execute(array($_POST['Pseudo']));
-
-						while ($Account_Data = $Account_Data_Query->fetch())
-						{	
-							$ID = $Account_Data['Account_ID'];
-							$bdd->exec("INSERT INTO Caranille_Inventory VALUES('', '$ID', '1', '1', 'Yes')");
-							$bdd->exec("INSERT INTO Caranille_Inventory VALUES('', '$ID', '2', '1', 'Yes')");
-							$bdd->exec("INSERT INTO Caranille_Inventory VALUES('', '$ID', '3', '1', 'Yes')");
-							$bdd->exec("INSERT INTO Caranille_Inventory VALUES('', '$ID', '4', '1', 'Yes')");
-							$bdd->exec("INSERT INTO Caranille_Inventory VALUES('', '$ID', '5', '1', 'Yes')");
-						}
-						$Account_Data_Query->closeCursor();
-
-						echo "$Register_8";
-					}
-					else
-					{
-						echo "$Register_9";
-					}
 				}
 				else
 				{
